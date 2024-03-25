a434a4b529ba66dd700b106d2c805b0569031cd7
a.brenga <antobrg90@hotmail.it>
2024-03-25 13:48:27 +0100

    feat: finished the exercise

--- file.js
+++ file.js
@@ -30,26 +30,25 @@ function creaRegistroUtenti(nome, cognome, eta) {
 
 
 btnInp.addEventListener("click", (e) => {
-    let studentegiapresente = false;
-    for (let i = 0; i <= studenti.length; i++) {
-        if (studentegiapresente) {
-            break;
-        } else {
-            studenti.push({
-                nome: nome.value,
-                cognome: cognome.value,
-                eta: eta.value
-            });
-            studentegiapresente = true;
+    const studente = {
+        nome: nome.value,
+        cognome: cognome.value,
+        eta: eta.value
+    };
 
-        };
-
+    let ePresente = studentePresente(studente);
+    if (ePresente) {
+        alert("alunno gia presente");
+    } else {
+        studenti.push(studente);
         reset(alunniLista)
         studenti.forEach(studente => {
             creaRegistroUtenti(studente.nome, studente.cognome, studente.eta);
 
         });
     }
+
+
 });
 
 
@@ -57,4 +56,13 @@ function reset(id) {
     id.innerHTML = " ";
 }
 
+function studentePresente(studente) {
+    for (let i = 0; i < studenti.length; i++) {
+        const studenteCorrente = studenti[i];
+        if (studenteCorrente.nome == studente.nome && studenteCorrente.cognome == studente.cognome && studenteCorrente.eta == studente.eta) {
+            return true;
 
+        }
+    } return false;
+
+}
